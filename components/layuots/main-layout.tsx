b1896ad0f88c56c4ba5d86f9cbb4255f62b3d572
Daniil Surin <wkingsurin@gmail.com>
2026-08-27 16:40:21 +0500

    feat(components): create MainLayout

--- components/layuots/main-layout.tsx
+++ components/layuots/main-layout.tsx
@@ -1,18 +1,22 @@
 import { ChartNoAxesCombined, LucideIcon, SquareUserRound } from "lucide-react";
 import Aside from "../structure/aside";
-import Dashboard from "../structure/dashboard";
 import Main from "../structure/main";
+import { ReactNode } from "react";
 
-export default function MainLayout() {
+interface MainProps {
+  children: ReactNode;
+}
+
+export default function MainLayout({ children }: MainProps) {
   const ASIDE_DATA: { label: string; icon: LucideIcon; href: string }[] = [
-    { label: "Contacts", icon: SquareUserRound, href: "clients" },
+    { label: "Clients", icon: SquareUserRound, href: "clients" },
     { label: "Statistics", icon: ChartNoAxesCombined, href: "statistics" },
   ];
 
   return (
     <Main className="flex gap-4">
       <Aside data={ASIDE_DATA} />
-      <Dashboard />
+      {children}
     </Main>
   );
 }
