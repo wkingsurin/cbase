a480eba55902837c88727d40d1037a880c53b7b4
Daniil Surin <wkingsurin@gmail.com>
2026-08-28 18:05:37 +0500

    feat(ui): add Checkbox component

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -70,7 +70,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0 border-r-[0.5px] border-gray/25",
+        "h-10 px-3 text-left align-middle font-medium whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0 border-r-[0.5px] border-gray/25",
         className
       )}
       {...props}
